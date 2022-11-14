6f23a60c640b39b02a80cfafda0216af5bf01bc3
Mika Mirzanasiri <mika-mm@hotmail.com>
2022-11-14 19:35:02 +0000

    Add project files.

---
+++ FinallyBlockExceptionHandling/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace FinallyBlockExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstLine = "No first line was read";
+            try
+            {
+                var lines = File.ReadAllLines(args[0]);
+                firstLine = (lines.Length > 0) ? lines[0] : "The file was empty";
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.Error.WriteLine("Please specify a filename.");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.Error.WriteLine("Unable to find file {0}", args[0]);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.Error.WriteLine("File {0} not accessible: {1}", args[0], ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine(firstLine);
+            }
+        }
+    }
+}
